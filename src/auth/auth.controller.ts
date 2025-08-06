b29f041473f5c2dbe23c16c75dd84b8679539e6e
Web developer <150703003+mirkamolhomidov@users.noreply.github.com>
2025-08-06 17:16:08 +0500

    updated log

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,24 +4,22 @@ import {
   InternalServerErrorException,
   Post,
   Res,
-} from '@nestjs/common';
-import { Response } from 'express';
-import { AuthService } from './auth.service';
-import { LoginDto, RegisterDto } from './dto/auth.dto';
+} from '@nestjs/common'
+import { Response } from 'express'
+import { AuthService } from './auth.service'
+import { LoginDto, RegisterDto } from './dto/auth.dto'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
   @Post('register')
   async registerController(@Body() registerDto: RegisterDto) {
-    console.log(registerDto);
-
-    const data = await this.authService.register(registerDto);
+    const data = await this.authService.register(registerDto)
     return {
       success: true,
       message: "Ro'yxatdan muvaffaqiyatli o'tdingiz",
       data,
-    };
+    }
   }
   @Post('login')
   async loginController(
@@ -29,23 +27,23 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ) {
     const { access_token, refresh_token, plan, ...data } =
-      await this.authService.login(loginDto);
+      await this.authService.login(loginDto)
     res.cookie('access_token', access_token, {
       httpOnly: true,
       sameSite: 'lax',
       maxAge: 1000 * 120 * 60,
-    });
+    })
     res.cookie('refresh_token', refresh_token, {
       httpOnly: true,
       sameSite: 'lax',
       maxAge: 1000 * 240 * 60,
-    });
+    })
     return {
       success: true,
       message: 'Login successfull',
       data,
       plan,
-    };
+    }
   }
   @Post('logout')
   async logout(@Res({ passthrough: true }) res: Response) {
@@ -53,18 +51,18 @@ export class AuthController {
       res.clearCookie('access_token', {
         httpOnly: true,
         sameSite: 'lax',
-      });
+      })
       res.clearCookie('refresh_token', {
         httpOnly: true,
         sameSite: 'lax',
-      });
-      res.status(200);
+      })
+      res.status(200)
       return {
         success: true,
         message: 'Muvaffaqiyatli tizimdan chiqildi',
-      };
+      }
     } catch (error) {
-      throw new InternalServerErrorException(`Xatolik: ${error.message}`);
+      throw new InternalServerErrorException(`Xatolik: ${error.message}`)
     }
   }
 }
